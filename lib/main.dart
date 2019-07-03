4e64648b4603c74d7cc34bd5a5229b825fff5671
Julian <haab.laurin@gmail.com>
2019-07-03 15:47:43 +0200

    V.0.3
    Todo:
    Wetter: 1 Tag, symbol als Icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cookie_of_the_day/model/TodoModel.dart';
 import 'package:cookie_of_the_day/service/Service.dart';
-import 'package:cookie_of_the_day/pages/TodoOfTheDayPage.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 import 'model/TodoList.dart';
